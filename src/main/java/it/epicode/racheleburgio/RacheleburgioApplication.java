32f3df8a99a7a288126ed74341944bd8bffec2b0
RacheleBurgio <rachele.burgio32@gmail.com>
2025-02-14 09:32:16 +0100

    Initial commit

---
+++ src/main/java/it/epicode/racheleburgio/RacheleburgioApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.racheleburgio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacheleburgioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacheleburgioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/racheleburgio/RacheleburgioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.racheleburgio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacheleburgioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
